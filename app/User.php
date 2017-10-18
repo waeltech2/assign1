6d039c5930ef88b9132390d39732826146e37ff2
waeltech2 <u1470153@unimail.hud.ac.uk>
2017-10-18 16:21:23 +0100

    added user role

--- app/User.php
+++ app/User.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Role;
 
 class User extends Authenticatable
 {
@@ -26,4 +27,25 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+    public function roles()
+    {
+      return $this->belongsToMany('App\Role');
+    }
+
+    public function hasRole($role)
+    {
+      if ($this->roles()->where('name', $role)->first()) {
+        return true;
+      }
+      return false;
+    }
+
+    public function authorize($roles)
+    {
+      if ($this->hasRole($roles)) {
+        return true;
+      }
+        return view('user');
+
+    }
 }
